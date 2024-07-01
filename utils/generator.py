a54e6765cbdcc65ebb89f0b5d73cc51950c20e5d
Connor McCaffrey <connor.mccaffrey@kornferry.com>
2024-07-01 02:41:34 -0400

    updated generator

--- utils/generator.py
+++ utils/generator.py
@@ -1,19 +1,55 @@
 import numpy as np
 import pandas as pd
 
-def basic(m_range, f_range, ratio):
+def basic(m_range, f_range, gender_ratio):
+    # total sample N + M + F
     N = 10000
-    m_mean = np.average(m_range)
-    f_mean = np.average(f_range)
-    m_std = (np.max(m_range) - m_mean) / 3
-    f_std = (np.max(f_range) - f_mean) / 3
-    m_n = np.round(N*ratio)
-    f_n = N - m_n
 
-    m_pay = np.random.normal(m_mean, m_std, 50)
-    f_pay = np.random.normal(f_mean, f_std, 50)
+    # P(M)
+    p_M = gender_ratio / 100
 
-    #pay = np.concat((m_pay, f_pay), axis=None)
-    #sex = np.concat((np.repeat("Male", 50), np.repeat("Female", 50)), axis=None)
-    #df = pd.DataFrame(np.c_[pay, sex], columns=["pay", "sex"])
-    return m_pay, f_pay
+    # slice indices
+    start = int(N*p_M)
+    stop = start + N
+
+    # calculate parameters
+    ranges = np.array((f_range, m_range))
+    means = np.average(ranges, 1)
+    stds = (ranges[:, 1] - means) / 3
+
+    print(f"Ranges: {ranges}")
+    print(f"Means: {means}")
+    print(f"StDs: {stds}")
+
+    # draw samples
+    np.random.seed(42)
+    pay = np.random.normal(means, stds, [N, 2]).T.flatten().round(2)
+    sex = np.repeat(["Female", "Male"], N)
+
+    # create dataframe
+    df = pd.DataFrame({"pay": pay[start:stop],
+                       "sex": sex[start:stop]})
+    return df
+
+if __name__ == "__main__":
+
+    # import plotly
+    import plotly.express as px
+
+    df = pd.DataFrame({"test": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]})
+    df['percentile'] = df['test'].rank(pct=True) * 100
+    print(df)
+
+"""
+    # test generation methods
+    male_pay_range = [60000, 80000]
+    female_pay_range = [60000, 80000]
+    gender_ratio = 0.5
+
+    df = basic(male_pay_range, female_pay_range, gender_ratio)
+
+    fig = px.histogram(df, x='pay', color='sex',
+                        labels={'pay': 'Pay', 'sex': 'Sex'},
+                        marginal="box")
+    fig.show()
+"""
